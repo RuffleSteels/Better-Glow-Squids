3de6ef00bc831e572f548aba0ac44a7fc823cf8b
Lollypoplove08 <rafferty.smith@hotmail.com>
2021-07-31 13:47:13 +0200

    Adds potions of glowing

--- src/main/java/com/santapexie/santapexie_bettergs/BetterGlowSquidsMain.java
+++ src/main/java/com/santapexie/santapexie_bettergs/BetterGlowSquidsMain.java
@@ -2,10 +2,15 @@ package com.santapexie.santapexie_bettergs;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistry;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 
 public class BetterGlowSquidsMain implements ModInitializer {
+    public static final String MOD_ID = "santapexie_bettergs";
+
     @Override
     public void onInitialize() {
-
+        PotionRegistry.registerAll();
     }
 }
