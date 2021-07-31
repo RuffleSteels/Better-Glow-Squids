33e947a423f3eaeff4040207789a7100f7677b9c
Lollypoplove08 <rafferty.smith@hotmail.com>
2021-07-31 12:18:45 +0200

    Initial commit

---
+++ src/main/java/com/santapexie/santapexie_bettergs/BetterGlowSquidsMain.java
@@ -0,0 +1,11 @@
+package com.santapexie.santapexie_bettergs;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class BetterGlowSquidsMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
