436e939a13c294974b28e812ff708a5cead20c6a
Lollypoplove08 <rafferty.smith@hotmail.com>
2021-07-31 18:21:27 +0200

    Adds potions of glowing

---
+++ src/main/java/com/santapexie/santapexie_bettergs/client/BetterGlowSquidsClient.java
@@ -0,0 +1,13 @@
+package com.santapexie.santapexie_bettergs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.item.PotionItem;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+
+public class BetterGlowSquidsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/santapexie/santapexie_bettergs/mixin/StatusEffectsMixin.java
@@ -0,0 +1,23 @@
+package com.santapexie.santapexie_bettergs.mixin;
+
+import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import java.awt.*;
+
+@Mixin(StatusEffects.class)
+public class StatusEffectsMixin {
+    @SuppressWarnings("UnresolvedMixinReference")
+    @ModifyArg(
+            method = "<clinit>",
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=glowing")),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffect;<init>(Lnet/minecraft/entity/effect/StatusEffectType;I)V", ordinal = 0)
+    )
+    private static int modifyGlowingColor(int original) {
+        return new Color(255, 253, 0).getRGB();
+    }
+}
